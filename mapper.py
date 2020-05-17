70647f44e8c54733c0a4488797b0a1b8473353fd
LonnyC55 <ecoxlauf@alumni.mines.edu>
2020-05-17 21:21:32 +0000

    remove stopwords, remove punctuation, and remove uppercase letters

--- mapper.py
+++ mapper.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 import sys
+from sklearn.feature_extraction import stop_words
+import string
 
 # get all lines from stdin
 for line in sys.stdin:
+
     # remove leading and trailing whitespace
     line = line.strip()
+
+    # remove punctuation from the current line of text
+    line = line.translate(None, string.punctuation)
+
+    # change all uppercase characters to lowercase characters
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
+    # assign the sklearn stop_words set to stops variable
+    stops = set(stop_words.ENGLISH_STOP_WORDS)
+
+    # output tuples (word, 1) in tab-delimited format and ignore stop words
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stops:
+            print '%s\t%s' % (word, "1")
